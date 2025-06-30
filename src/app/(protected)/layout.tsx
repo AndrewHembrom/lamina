1144606f2d830c091a6b9c232b4958210cc79fce
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2025-06-30 21:27:42 +0530

    dahboard

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import React, { Children } from "react";
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
 type Props = {
   children: React.ReactNode;
@@ -7,7 +8,7 @@ type Props = {
 
 const SidebarLayout = ({ children }: Props) => {
   return (
-    <SidebarLayout>
+    <SidebarProvider>
       {/* <AppSidebar/> */}
       <main className="m-2 w-full">
         <div className="border-sidebar-border bg-sidebar flex items-center gap-2 rounded-md border p-2 px-4 shadow">
@@ -15,7 +16,12 @@ const SidebarLayout = ({ children }: Props) => {
           <div className="ml-auto"></div>
           <UserButton />
         </div>
+        <div className="h-4"></div>
+        {/* main content */}
+        <div className="border-sidebar-border bg-sidebar h-[calc(100vh - 6rem)] overflow-y-scroll rounded-md border p-4 shadow">
+          {children}
+        </div>
       </main>
-    </SidebarLayout>
+    </SidebarProvider>
   );
 };
