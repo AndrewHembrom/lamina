8a0e1c282ccd095cfa4ad36d4920b2ead965eb26
Andrew Joy Hembrom <135138539+AndrewHembrom@users.noreply.github.com>
2025-07-02 21:21:35 +0530

    create page and updated table

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,9 +1,15 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { z } from 'zod';
 
 export const projectRouter = createTRPCRouter({
-    createProject: protectedProcedure.input().mutation(async ({ ctx, input }) => {
-        ctx.user.userId;
-        console.log("hi");
+    createProject: protectedProcedure.input(
+        z.object({
+            name: z.string(),
+            githubUrl: z.string(),
+            githubToken: z.string().optional()
+        })
+    ).mutation(async ({ ctx, input }) => {
+        console.log("Input", input);
         return true;
     })
 })
